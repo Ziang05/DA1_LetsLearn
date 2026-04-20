e2dfc1a233efce5f90b7b89e6e118cfd5af203c7
Ziang <sang24964@gmail.com>
2026-04-20 10:23:06 +0700

    Add CRUD API for Sections service

---
+++ src/LetsLearn.UseCases/DTOs/SectionDTOs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsLearn.UseCases.DTOs
+{
+    public class SectionDTO
+    {
+        public Guid Id { get; set; }
+
+        public int Position { get; set; }
+
+        [Required(ErrorMessage = "Title cannot be empty")]
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public Guid CourseId { get; set; }
+
+        public List<TopicDTO>? Topics { get; set; }
+    }
+
+    public class SectionResponse
+    {
+        public Guid Id { get; set; }
+        public string CourseId { get; set; } = null!;
+        public int? Position { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public List<TopicResponse> Topics { get; set; } = new();
+    }
+
+    public class CreateSectionRequest
+    {
+        public string CourseId { get; set; } = null!;
+        public int? Position { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class UpdateSectionRequest
+    {
+        public Guid Id { get; set; }
+        public int? Position { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public List<TopicUpsertDTO>? Topics { get; set; }
+    }
+}
---
+++ src/LetsLearn.UseCases/ServiceInterfaces/ISectionService.cs
@@ -0,0 +1,16 @@
+﻿using LetsLearn.UseCases.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsLearn.UseCases.ServiceInterfaces
+{
+    public interface ISectionService
+    {
+        Task<SectionResponse> CreateSectionAsync(CreateSectionRequest request, CancellationToken ct = default);
+        Task<SectionResponse> GetSectionByIdAsync(Guid sectionId, CancellationToken ct = default);
+        Task<SectionResponse> UpdateSectionAsync(UpdateSectionRequest request, CancellationToken ct = default);
+    }
+}
